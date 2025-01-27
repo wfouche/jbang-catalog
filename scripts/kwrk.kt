3f3d2556da80789720adf9d79fa28ddf0e43569d
Werner Fouché <werner.fouche@gmail.com>
2025-01-27 07:52:49 +0200

    Renamed benchmark filenames

--- scripts/kwrk.kt
+++ scripts/kwrk.kt
@@ -26,8 +26,8 @@ val benchmarkConfig:String = """
     // Actions
     "actions": {
         "description": "kwrk",
-        "output_filename": "benchmark_output.json",
-        "report_filename": "benchmark_report.html",
+        "output_filename": "kwrk_output.json",
+        "report_filename": "kwrk_report.html",
         "user_class": "HttpUser",
         "user_params": {
             "baseURI": "__P_URL__",
