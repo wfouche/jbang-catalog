0e7f1a9371178b473c3988c6fd2637ee2b2b4d3e
Werner Fouché <werner.fouche@gmail.com>
2025-02-09 18:05:43 +0200

    Renamed jython class to jythonJpm

--- scripts/jythonJpm.java
+++ scripts/jythonJpm.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-public class jython 
+public class jythonJpm
 {
 
     public static void main(String[] args) throws IOException, InterruptedException  {
