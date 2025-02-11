1d67dbf9cb0de9336dd2dcc8ac769310461b22dd
Werner Fouché <werner.fouche@gmail.com>
2025-02-11 09:34:27 +0200

    Implemented all three Jython actions

--- scripts/tulip-cli.kt
+++ scripts/tulip-cli.kt
@@ -764,13 +764,17 @@ val runBenchCmdJython: String = """
 
 val JythonJava: String = """
     ///usr/bin/env jbang "${'$'}0" "${'$'}@" ; exit ${'$'}?
-
+    
     //DEPS org.python:jython-standalone:2.7.4
-    //DEPS io.github.wfouche.tulip:tulip-runtime:__TULIP_VERSION__
+    //DEPS io.github.wfouche.tulip:tulip-runtime:2.1.5
+    //DEPS org.springframework.boot:spring-boot-starter-web:3.4.2
+    //DEPS org.slf4j:slf4j-api:2.0.16
+    //DEPS ch.qos.logback:logback-core:1.5.16
+    //DEPS ch.qos.logback:logback-classic:1.5.16
     //JAVA 21
-
+    
     import org.python.util.jython;
-
+    
     public class Jython {
         public static void main(String... args) {
             jython.main(args);
@@ -781,36 +785,75 @@ val JythonJava: String = """
 
 val JythonBenchmark: String = """
     from __future__ import print_function
-
+    
     import io.github.wfouche.tulip.api.TulipUser as TulipUser
     import io.github.wfouche.tulip.api.TulipUserFactory as TulipUserFactory
     import io.github.wfouche.tulip.api.TulipApi as TulipApi
-
+    import org.springframework.web.client.RestClient as RestClient
+    import org.springframework.web.client.RestClientException as RestClientException
+    import org.springframework.http.client.SimpleClientHttpRequestFactory as SimpleClientHttpRequestFactory
+    import java.util.concurrent.ThreadLocalRandom as ThreadLocalRandom
+    import java.lang.String
+    
+    restClient = None
+    
     class HttpUser(TulipUser):
-
+    
         def __init__(self, userId, threadId):
             TulipUser.__init__(self, userId, threadId)
-
+    
         def onStart(self):
+            if self.userId == 0:
+                print("Jython")
+                print("Initializing static data")
+                connectTimeout = int(self.getUserParamValue("connectTimeoutMillis"))
+                readTimeout = int(self.getUserParamValue("readTimeoutMillis"))
+                factory = SimpleClientHttpRequestFactory()
+                factory.setConnectTimeout(connectTimeout)
+                factory.setReadTimeout(readTimeout)
+                url = self.getUserParamValue("protocol") + "://" + self.getUserParamValue("host")
+                global restClient
+                restClient = RestClient.builder().requestFactory(factory).baseUrl(url).build()
             return True
-
+    
         def action1(self):
-            return True
-
+            rc = False
+            try:
+                id = ThreadLocalRandom.current().nextInt(100) + 1
+                rsp = restClient.get().uri("/posts/{id}", id).retrieve().body(java.lang.String)
+                rc = (rsp is not None and len(rsp) > 2)
+            except RestClientException:
+                pass
+            return rc
+    
         def action2(self):
-            return True
-
+            rc = False
+            try:
+                id = ThreadLocalRandom.current().nextInt(500) + 1
+                rsp = restClient.get().uri("/comments/{id}", id).retrieve().body(java.lang.String)
+                rc = (rsp is not None and len(rsp) > 2)
+            except RestClientException:
+                pass
+            return rc
+    
         def action3(self):
-            return True
-
+            rc = False
+            try:
+                id = ThreadLocalRandom.current().nextInt(200) + 1
+                rsp = restClient.get().uri("/todos/{id}", id).retrieve().body(java.lang.String)
+                rc = (rsp is not None and len(rsp) > 2)
+            except RestClientException:
+                pass
+            return rc
+    
         def onStop(self):
             return True
-
+    
     class UserFactory(TulipUserFactory):
-
+    
         def getUser(self, userId, className, threadId):
             return HttpUser(userId, threadId)
-
+    
     TulipApi.runTulip("benchmark_config.jsonc", UserFactory())
 
 """.trimIndent()
