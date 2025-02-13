7614b9192f20721ee0fd7d88abfdb37468929054
Werner Fouché <werner.fouche@gmail.com>
2025-02-13 21:09:49 +0200

    Add clojure command

---
+++ scripts/Clojure.java
@@ -0,0 +1,12 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS org.clojure:clojure:1.12.0
+//JAVA 21
+
+import clojure.main;
+
+public class Clojure {
+    public static void main(String[] args) {
+        main.main(args);
+    }
+}
