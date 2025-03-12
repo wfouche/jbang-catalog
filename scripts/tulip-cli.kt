11e1471ac505360690f5ac481cd2160af7bf4c39
Werner Fouché <werner.fouche@gmail.com>
2025-03-12 17:45:46 +0200

    Renamed serviceCall to http_GET for Java benchmark

--- scripts/tulip-cli.kt
+++ scripts/tulip-cli.kt
@@ -266,30 +266,30 @@ val javaUser: String = """
         // Action 1: GET /posts/{id}
         public boolean action1() {
             int id = ThreadLocalRandom.current().nextInt(100)+1;
-            return serviceCall("/posts/{id}", id);
+            return http_GET("/posts/{id}", id);
         }
 
         // Action 2: GET /comments/{id}
         public boolean action2() {
             int id = ThreadLocalRandom.current().nextInt(500)+1;
-            return serviceCall("/comments/{id}", id);
+            return http_GET("/comments/{id}", id);
         }
 
         // Action 3: GET /todos/{id}
         public boolean action3() {
             int id = ThreadLocalRandom.current().nextInt(200)+1;
-            return serviceCall("/todos/{id}", id);
+            return http_GET("/todos/{id}", id);
         }
 
         public boolean onStop() {
             return true;
         }
 
-        private boolean serviceCall(String uri, int id) {
+        private boolean http_GET(String uri, Object... uriVariables) {
             boolean rc;
             try {
                 String rsp = client.get()
-                    .uri(uri, id)
+                    .uri(uri, uriVariables)
                     .retrieve()
                     .body(String.class);
                 rc = (rsp != null && rsp.length() > 2);
