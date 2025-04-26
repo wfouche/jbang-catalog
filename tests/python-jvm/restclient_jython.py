37faa54b571e22ab3f577d08ab3f08b39d227c56
Werner Fouché <werner.fouche@gmail.com>
2025-04-26 14:05:16 +0200

    restclient_jython.py - improved example code

--- tests/python-jvm/restclient_jython.py
+++ tests/python-jvm/restclient_jython.py
@@ -9,7 +9,7 @@
 # requires-jython = "==2.7.4"
 # requires-java = ">=21"
 # dependencies = [
-#   "org.springframework.boot:spring-boot-starter-web:3.4.4"
+#   "org.springframework.boot:spring-boot-starter-web:3.4.5"
 # ]
 # [java]
 #   runtime-options = "-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational"
@@ -20,12 +20,15 @@
 import java.lang.String as String
 import org.springframework.web.client.RestClient as RestClient
 
-def restApiCall(uri, id):
-    restClient = RestClient.create()
+restClient = RestClient.create()
+
+def restApiCall(uri, id):    
     rsp = restClient.get().uri(uri, id).retrieve().body(String)
     print(rsp)
 
 def main():
     restApiCall("https://jsonplaceholder.typicode.com/todos/{id}", 1)
+    restApiCall("https://jsonplaceholder.typicode.com/todos/{id}", 2)
+    restApiCall("https://jsonplaceholder.typicode.com/todos/{id}", 3)
 
 main()
