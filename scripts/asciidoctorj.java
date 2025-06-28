20b3662602666122ab3e373ccfec4a0eb6037759
Werner Fouché <werner.fouche@gmail.com>
2025-06-28 15:41:21 +0200

    asciidoctorj.java - added dependencies

--- scripts/asciidoctorj.java
+++ scripts/asciidoctorj.java
@@ -4,9 +4,12 @@
 //DEPS org.asciidoctor:asciidoctorj-cli:3.0.0
 //DEPS org.asciidoctor:asciidoctorj-epub3:2.1.3
 //DEPS org.asciidoctor:asciidoctorj-diagram:3.0.1
+//DEPS org.asciidoctor:asciidoctorj-diagram-batik:1.17
+//DEPS org.asciidoctor:asciidoctorj-diagram-ditaamini:1.0.3
 //DEPS org.asciidoctor:asciidoctorj-diagram-plantuml:1.2025.3
+//DEPS org.asciidoctor:asciidoctorj-diagram-jsyntrax:1.38.2
 //DEPS org.asciidoctor:asciidoctorj-pdf:2.3.19
-//DEPS org.asciidoctor:asciidoctorj-revealjs:5.1.0
+//DEPS org.asciidoctor:asciidoctorj-revealjs:5.2.0
 //DEPS com.beust:jcommander:1.82
 //DEPS org.jruby:jruby-complete:9.4.8.0
 
