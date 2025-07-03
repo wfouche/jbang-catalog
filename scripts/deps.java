9e393a124717f61e9984d2776ea25b800c390f40
Werner Fouché <werner.fouche@gmail.com>
2025-07-03 21:42:48 +0200

    Updated deps.java

--- scripts/deps.java
+++ scripts/deps.java
@@ -3,13 +3,13 @@
 //DEPS dev.jbang:jash:0.0.3
 //DEPS com.fasterxml.jackson.core:jackson-databind:2.19.1
 //DEPS info.picocli:picocli:4.7.7
-//DEPS eu.maveniverse.maven.plugins:toolbox:0.11.2
 
-// NOTE: The eu.maveniverse.maven.plugins:toolbox dependency is not used directly.
-// We only include it here so that running deps.java on itself will let us know if
-// a newer version of toolbox is available. If a newer toolbox is available, then
-// update the  URL pointing to the toolbox jar. See String variable toolboxJar.
-// Also update //DEPS eu.maveniverse.maven.plugins:toolbox:<version>
+//DEPS eu.maveniverse.maven.plugins:toolbox:0.11.2
+//DEPS org.apache.maven:maven-plugin-api:3.9.10
+//DEPS org.apache.maven:maven-settings:3.9.10
+//DEPS eu.maveniverse.maven.mima.runtime:standalone-static:2.4.29
+
+//DEPS org.slf4j:slf4j-simple:2.0.17
 
 import static dev.jbang.jash.Jash.*;
 
@@ -40,20 +40,27 @@ public class deps implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         if (script.equals("")) {
+            // Return failure if no JBang script file is supplied
             return 1;
         }
-        String toolboxJar = "https://repo.maven.apache.org/maven2/eu/maveniverse/maven/plugins/toolbox/0.11.2/toolbox-0.11.2-cli.jar";
+
+        // Get dependencies
         String jbang_launch_cmd = System.getenv("JBANG_LAUNCH_CMD");
         String dependencies = $(jbang_launch_cmd + " info tools --quiet --select=dependencies " + script).get();
         JsonNode deps = new ObjectMapper().readTree(dependencies);
+
+        // Build the gavList
         List<String> gavList = new LinkedList<>();
         deps.forEach(
                 dep -> {
                     gavList.add(dep.asText());
                 });
-        String result = $(jbang_launch_cmd + " trust add " + toolboxJar).get();
-        String command = jbang_launch_cmd + " run " + toolboxJar + " versions " + String.join(",", gavList);
-        $(command).stream().forEach(System.out::println);
+
+        // Check for version updates
+        String[] toolbox_args = {"versions", String.join(",", gavList)};
+        eu.maveniverse.maven.toolbox.plugin.CLI.main(toolbox_args);
+
+        // Return success
         return 0;
     }
 
