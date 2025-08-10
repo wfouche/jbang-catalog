30d0b90352cab59df4c5676bf88ff50e4c01b961
Werner Fouché <werner.fouche@gmail.com>
2025-08-10 09:06:52 +0200

    tulip-cli-dev: simplified using ThreadLocalRandom

--- scripts/TulipCli.java
+++ scripts/TulipCli.java
@@ -204,6 +204,8 @@ public class TulipCli {
 
     public class JavaHttpUser extends HttpUser {
 
+        private ThreadLocalRandom random = ThreadLocalRandom.current();
+    
         public JavaHttpUser(int userId, int threadId) {
             super(userId, threadId);
         }
@@ -219,30 +221,30 @@ public class TulipCli {
 
         // Action 1: GET /posts/{id}
         public boolean action1() {
-            int id = ThreadLocalRandom.current().nextInt(100)+1;
+            int id = random.nextInt(100)+1;
             return !http_GET("/posts/{id}", id).isEmpty();
         }
 
         // Action 2: GET /comments/{id}
         public boolean action2() {
-            int id = ThreadLocalRandom.current().nextInt(500)+1;
+            int id = random.nextInt(500)+1;
             return !http_GET("/comments/{id}", id).isEmpty();
         }
 
         // Action 3: GET /todos/{id}
         public boolean action3() {
-            int id = ThreadLocalRandom.current().nextInt(200)+1;
+            int id = random.nextInt(200)+1;
             return !http_GET("/todos/{id}", id).isEmpty();
         }
 
         public boolean onStop() {
             return true;
         }
-        
+    
         // Logger
         private static final Logger logger = LoggerFactory.getLogger(JavaHttpUser.class);
 
-    }    
+    }
     """.stripIndent();
 
     static String runBenchShJava = """
