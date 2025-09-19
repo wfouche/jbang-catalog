56b82d4130d152c0b8d9ce9be021073ed3e43db3
Werner Fouché <werner.fouche@gmail.com>
2025-09-19 13:29:15 +0200

    Support Groovy 5.0.1

--- scripts/TulipCli.java
+++ scripts/TulipCli.java
@@ -560,7 +560,7 @@ public class TulipCli {
     //DEPS org.springframework.boot:spring-boot-starter-web:3.5.5
     //SOURCES GroovyHttpUser.groovy
     //JAVA 21
-    //GROOVY 5.0.0
+    //GROOVY 5.0.1
     //RUNTIME_OPTIONS __TULIP_JAVA_OPTIONS__
     //COMPILE_OPTIONS --tolerance=5
     //FILES ../../benchmark_config.json
