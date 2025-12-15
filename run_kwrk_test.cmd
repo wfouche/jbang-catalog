dccb8198ab436d69534521cec43f4d931fb89366
Werner Fouché <werner.fouche@gmail.com>
2025-12-15 22:11:12 +0200

    New run kwrk test script (cmd)

--- run_kwrk_test.cmd
+++ run_kwrk_test.cmd
@@ -1,3 +1,3 @@
-jbang run kwrk@wfouche --url http://jsonplaceholder.typicode.com/posts/1 --rate 10
+call jbang run kwrk@wfouche --url http://jsonplaceholder.typicode.com/posts/1 --rate 10
 
 start .\kwrk_test_report.html
