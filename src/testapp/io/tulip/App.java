81fca4c88ed20e0654d76370dfcb6cde8d4c4eeb
Werner Fouché <werner.fouche@gmail.com>
2026-02-05 21:09:05 +0200

    testapp: upgrade to Tulip 2.2.1

--- src/testapp/io/tulip/App.java
+++ src/testapp/io/tulip/App.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.github.wfouche.tulip:tulip-runtime:2.2.0
+//DEPS io.github.wfouche.tulip:tulip-runtime:2.2.1
 //SOURCES JavaHttpUser.java
 //JAVA 21
 //FILES ../../benchmark_config.json
