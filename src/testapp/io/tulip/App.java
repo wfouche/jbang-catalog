e6a1b2adae9ee2d56f0f88b4385da593279655eb
Werner Fouché <werner.fouche@gmail.com>
2026-02-05 18:58:24 +0200

    test-app: test application with //FILES and //SOURCES

---
+++ src/testapp/io/tulip/App.java
@@ -0,0 +1,19 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.github.wfouche.tulip:tulip-runtime:2.2.1
+//SOURCES JavaHttpUser.java
+//JAVA 21
+//FILES ../../benchmark_config.json
+//FILES ../../logback.xml
+//RUNTIME_OPTIONS -XX:+IgnoreUnrecognizedVMOptions
+//RUNTIME_OPTIONS --enable-native-access=ALL-UNNAMED
+//RUNTIME_OPTIONS --sun-misc-unsafe-memory-access=allow
+
+package io.tulip;
+
+import io.github.wfouche.tulip.api.TulipApi;
+
+public class App {
+   public static void main(String[] args) {
+      TulipApi.generateReport(TulipApi.runTulip("benchmark_config.json"));
+   }
+}
