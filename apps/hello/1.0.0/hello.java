7f83d2182c4cbc10036f3bc2ab4d4093d4d7dab5
Werner Fouché <werner.fouche@gmail.com>
2026-02-13 20:23:50 +0200

    hello 1.0.0

--- apps/hello/1.0.0/hello.java
+++ apps/hello/1.0.0/hello.java
@@ -1,31 +1,9 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-//DEPS info.picocli:picocli:4.7.7
-
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
-
-import java.util.concurrent.Callable;
-
-@Command(
-        name = "hello", mixinStandardHelpOptions = true,
-        version = "${sys:jbang.app.version:-0.0.0}",
-        description = "")
-public class hello implements Callable<Integer> {
-
-    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
-    private String greeting;
+public class hello {
 
     public static void main(String... args) {
-        int exitCode = new CommandLine(new hello()).execute(args);
-        System.exit(exitCode);
-    }
-
-    @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        System.out.println("Hello " + greeting);
-        return 0;
+        System.out.println("Hello World! , 1.0.0")
     }
 
 }
