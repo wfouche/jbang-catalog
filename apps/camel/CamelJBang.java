eb43d6d5ee6c4da51d3ff2953d63d14b511bcd0e
Werner Fouché <werner.fouche@gmail.com>
2026-02-14 18:16:14 +0200

    Camel scripts

---
+++ apps/camel/CamelJBang.java
@@ -0,0 +1,18 @@
+//JAVA 17+
+//REPOS central=https://repo1.maven.org/maven2,apache-snapshot=https://repository.apache.org/content/groups/snapshots/
+//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.17.0}@pom
+//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.17.0}
+//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.17.0}
+
+package main;
+
+import org.apache.camel.dsl.jbang.core.commands.CamelJBangMain;
+
+/**
+ * Main to run CamelJBang
+ */
+public class CamelJBang {
+
+    public static void main(String... args) {
+        CamelJBangMain.run(args);
+    }
